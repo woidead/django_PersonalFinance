e153e4e8a4edcec9a6ed6607cd2fee97cc715922
woidead <abikbest05@gmail.com>
2024-03-04 16:37:23 +0600

    created listwiew for incomes

--- apps/Incomes/views.py
+++ apps/Incomes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Income
 
-# Create your views here.
+def incomes_list(request):
+    incomes = Income.objects.all()
+    return render(request, 'incomes/incomes_list.html', {'incomes': incomes})
