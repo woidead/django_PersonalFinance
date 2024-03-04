c4b820a66f263123a17242f79a141ede5e1393ee
woidead <abikbest05@gmail.com>
2024-03-04 16:18:07 +0600

    new model and migration

--- apps/Incomes/models.py
+++ apps/Incomes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Income(models.Model):
+    source = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.source} - {self.amount}"
