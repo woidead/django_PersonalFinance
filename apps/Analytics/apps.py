964a5ac890de4c635277e522d3ca8e376a52059c
woidead <abikbest05@gmail.com>
2024-03-04 16:17:40 +0600

    изменено распложение приложений

--- apps/Analytics/apps.py
+++ apps/Analytics/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Analytics'
+    name = 'apps.Analytics'
--- apps/Incomes/apps.py
+++ apps/Incomes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class IncomesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Incomes'
+    name = 'apps.Incomes'
