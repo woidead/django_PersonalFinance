a011a38ac73ad94978dd8c08c920693b32c25551
woidead <abikbest05@gmail.com>
2024-03-04 17:31:02 +0600

    created and registered path for IncomeCreateView

--- apps/Incomes/urls.py
+++ apps/Incomes/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('incomes/', views.incomes_list, name = 'incomes_list')
+    path('incomes/', views.incomes_list, name = 'incomes_list'),
+    path('incomes/add/', views.IncomeCreateView.as_view(), name='create_income')
 ]
--- apps/Incomes/views.py
+++ apps/Incomes/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import IncomeForm
 from .models import Income
 
 def incomes_list(request):
     incomes = Income.objects.all()
     return render(request, 'incomes_list.html', {'incomes': incomes})
+
+class IncomeCreateView(CreateView):
+    model = Income
+    form_class = IncomeForm
+    template_name = 'create_income.html'
+    success_url = reverse_lazy('incomes_list')
