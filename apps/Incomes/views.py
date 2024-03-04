b913513788f3345a9118f47e74afb5ffc791abbb
woidead <abikbest05@gmail.com>
2024-03-04 17:58:13 +0600

    created IncomeDetailView

--- apps/Incomes/views.py
+++ apps/Incomes/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .forms import IncomeForm
 from .models import Income
+from django.views.generic.detail import DetailView
 
 def incomes_list(request):
     incomes = Income.objects.all()
@@ -13,3 +14,8 @@ class IncomeCreateView(CreateView):
     form_class = IncomeForm
     template_name = 'create_income.html'
     success_url = reverse_lazy('incomes_list')
+
+class IncomeDetailView(DetailView):
+    model = Income
+    context_object_name = 'income'
+    template_name = 'income_detail.html'
