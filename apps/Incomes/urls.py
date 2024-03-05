958c12709d756de579b8e4f35b45ad44fc12c652
woidead <abikbest05@gmail.com>
2024-03-05 14:52:37 +0600

    update

--- apps/Incomes/urls.py
+++ apps/Incomes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('incomes/', views.incomes_list, name = 'incomes_list'),
-    path('incomes/add/', views.IncomeCreateView.as_view(), name='create_income')
+    path('incomes/add/', views.IncomeCreateView.as_view(), name='create_income'),
+    path('income/<int:pk>/', views.IncomeDetailView.as_view(), name="income_detail_view"),
 ]
